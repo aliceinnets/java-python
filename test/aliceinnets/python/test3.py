89920b5b90e2b6565027c8ff156bb6e692f73099
aliceinnets <aliceinnets@gmail.com>
2018-04-15 07:28:26 -0700

    committed scripts

--- test/aliceinnets/python/test3.py
+++ test/aliceinnets/python/test3.py
@@ -1,5 +1,5 @@
 #Auto generated python script
-#generated at Sun Apr 15 05:30:21 PDT 2018
+#generated at Sun Apr 15 07:08:02 PDT 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,6 +9,6 @@ import matplotlib.pyplot as plt
 print('hello, world')
 a = np.linspace(0, 10, 100)
 print(5.0)
-c = np.linsapce(1.0, 5.0, )
+c = np.linsapce(1.0, 5.0, '')
 print(b)
 print(c)
